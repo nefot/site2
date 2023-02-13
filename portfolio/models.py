b932544e2b366915ae908c12dc0c7810d0cdf91e
Артем <54632529+nefot@users.noreply.github.com>
2023-02-13 21:08:02 +0300

    licende

--- portfolio/models.py
+++ portfolio/models.py
@@ -5,8 +5,8 @@ from django.utils import timezone
 
 class Project(models.Model):
     title = models.CharField(max_length=100, verbose_name="Имя качка")
-    subtitle = models.CharField(max_length=255, verbose_name="Характеристика качка")
-    functions = models.CharField(max_length=100, verbose_name="Функции качка", default="Бездарь")
+    subtitle = models.CharField(max_length=500, verbose_name="Характеристика качка")
+    functions = models.CharField(max_length=500, verbose_name="Функции качка", default="Бездарь")
     image = models.ImageField(upload_to='portfolio/images/', verbose_name="Фото качка")
     time = models.DateTimeField(auto_now=False, default=timezone.now)
     is_published = models.BooleanField(default=True)
