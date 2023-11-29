a87da8e4437aa601095b2f8fb8de8fee97384d04
artyom <artyom27000@mail.ru>
2023-11-29 14:28:16 +0300

    17

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -5,12 +5,12 @@ from django.contrib import admin
 from .models import *
 
 
-
 class Person(admin.ModelAdmin):
-    list_display = ['title', 'subtitle', 'time', 'is_published',"money"]
-    list_display_links = ['title','time']
-    search_fields = ('title','time')
+    list_display = ['title', 'subtitle', 'time', 'is_published', "money"]
+    list_display_links = ['title', 'time']
+    search_fields = ('title', 'time')
     list_editable = ['is_published']
+
 
 # title = models.CharField(max_length=100, verbose_name="Имя качка")
 #     subtitle = models.CharField(max_length=255, verbose_name="Характеристика качка")
--- portfolio/models.py
+++ portfolio/models.py
@@ -16,6 +16,27 @@ def make_thumbnail(image, size=(100, 100)):
     return thumbnail
 
 
+class Almanac(models.Model):
+    task = models.CharField(max_length=100, verbose_name = 'Название предмета')
+    # name = models.CharField(max_length=100, verbose_name = 'Имя преподавателя')
+    # time = models.DateTimeField('время пары')
+
+    monday   = models.BooleanField(default = 0, verbose_name = 'monday'  )
+    tuesday  = models.BooleanField(default = 0, verbose_name = 'tuesday' )
+    wensday  = models.BooleanField(default = 0, verbose_name = 'wensday' )
+    thursdy  = models.BooleanField(default = 0, verbose_name = 'thursdy' )
+    friday   = models.BooleanField(default = 0, verbose_name = 'friday'  )
+    saturday = models.BooleanField(default = 0, verbose_name = 'saturday')
+    sunday   = models.BooleanField(default = 0, verbose_name = 'sunday'  )
+
+    def __str__(self):
+        return self.task
+
+    class Meta:
+        verbose_name = "Расписание"
+        verbose_name_plural = "Расписание недели"
+
+
 class Project(models.Model):
     title = models.CharField(max_length=100, verbose_name="Имя качка")
     subtitle = models.CharField(max_length=500, verbose_name="Характеристика качка")
